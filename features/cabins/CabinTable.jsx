ee828fb1de58fbc271a9cf6afdcbcd1f5a910d9a
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-03 02:16:37 -0500

    Add sorting functionality to CabinTable

--- features/cabins/CabinTable.jsx
+++ features/cabins/CabinTable.jsx
@@ -14,8 +14,10 @@ function CabinTable() {
   const filterValue = searchParams.get('discount');
   console.log(filterValue);
 
+  /**
+   * FILTER CABINS
+   */
   let filteredCabins;
-
   switch (filterValue) {
     case 'no-discount':
       filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
@@ -30,6 +32,16 @@ function CabinTable() {
       filteredCabins = cabins;
   }
 
+  /**
+   * SORT CABINS
+   */
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
@@ -42,7 +54,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
