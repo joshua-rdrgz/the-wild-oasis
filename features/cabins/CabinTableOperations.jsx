928aa668bbbb33e97a9847f47a4a587ba1d98c4a
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-03 02:16:24 -0500

    Add SortBy component to CabinTableOperations

--- features/cabins/CabinTableOperations.jsx
+++ features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from '../../ui/TableOperations';
 import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
 
 function CabinTableOperations() {
   return (
@@ -14,6 +15,26 @@ function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No Discount' },
           { value: 'with-discount', label: 'With Discount' },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-desc', label: 'Sort by name (Z-A)' },
+          { value: 'regularPrice-asc', label: 'Sort by price (lowest first)' },
+          {
+            value: 'regularPrice-desc',
+            label: 'Sort by price (highest first)',
+          },
+          {
+            value: 'maxCapacity-asc',
+            label: 'Sort by capacity (lowest first)',
+          },
+          {
+            value: 'maxCapacity-desc',
+            label: 'Sort by capacity (highest first)',
+          },
         ]}
       />
     </TableOperations>
