8749f1a166d4e274423ae113b47d2b885e19515e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-05 06:28:09 -0500

    return numDays in useRecentStays hook

--- features/dashboard/useRecentStays.js
+++ features/dashboard/useRecentStays.js
@@ -21,5 +21,5 @@ export function useRecentStays() {
     (stay) => stay.status === 'checked-in' || stays.status === 'checked-out'
   );
 
-  return { stays, isLoading, confirmedStays };
+  return { stays, isLoading, confirmedStays, numDays };
 }
