d2b3c8f74fb54bf16a9ff1baf7ebc04de543f48a
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-05 06:28:47 -0500

    Add Stats component to DashboardLayout

--- features/dashboard/DashboardLayout.jsx
+++ features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import Spinner from '../../ui/Spinner';
 import { useRecentBookings } from './useRecentBookings';
 import { useRecentStays } from './useRecentStays';
+import { useCabins } from '../cabins/useCabins';
+import Stats from './Stats';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -12,13 +14,25 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoadingStays } = useRecentStays();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoadingStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
 
-  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins)
+    return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today's activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
